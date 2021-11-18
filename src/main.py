41901e11ddcaee868250be1630f09cdc7474455d
Håvard Melheim <haavame@stud.ntnu.no>
2021-11-18 14:50:25 +0100

    More complete functionality

--- src/main.py
+++ src/main.py
@@ -4,11 +4,40 @@ import torch
 from torch.utils.data import DataLoader
 from plot import DataPlotter
 from dataHandler import DataHandler
-from network import FCNN, train
+from network import Trainer
 
 def main():
-	datahandler = DataHandler("../data/well_data.csv", index_col=0, seed=12345)
+	datahandler = DataHandler("../data/well_data.csv", index_col=0, seed=12345, test_indices=(2000, 2500), validation_frac=0.1)
+	# datahandler.generate_data_sets(test_indices=(2000, 2500), validation_frac=0.1)
 
+	#draw_plots(datahandler)
+
+	input_cols = ['CHK', 'PWH', 'PDC', 'TWH', 'FGAS', 'FOIL']
+	output_cols = ['QTOT']
+
+	datahandler.set_input_cols(input_cols)
+	datahandler.set_output_cols(output_cols)
+
+	layers = [len(input_cols), 50, 50, len(output_cols)]
+
+	n_epochs = 100
+	n_steps = 5
+	lr = 0.001
+	l2_reg = 0.001  # 10
+	l1_reg = 0.001  # 10
+	patience = 10
+	retrain = False
+
+	trainer = Trainer(layers=layers, data=datahandler, early_stopping=True)
+
+	trainer.train(n_epochs=n_steps, lr=lr, l2_reg=l2_reg, l1_reg=l1_reg, patience=patience, retrain=retrain)
+
+	mse_value, mae_value, mape_value = trainer.evaluate(mode='test')
+
+	print_evaluation(mse_value, mae_value, mape_value)
+
+
+def draw_plots(datahandler: DataHandler):
 	df = datahandler.get_dataframe()
 
 	plotter = DataPlotter(subplots=True, subplot_size=(2,2))
@@ -21,8 +50,6 @@ def main():
 
 	plotter.show()
 
-	datahandler.generate_data_sets(test_indices=(2000, 2500), validation_frac=0.1)
-
 	scatter = DataPlotter(subplots=False)
 	scatter.fill_scatter(indices=datahandler.get_train_set().index, values=datahandler.get_train_set()['QTOT'], color='black', label='Train')
 	scatter.fill_scatter(indices=datahandler.get_val_set().index, values=datahandler.get_val_set()['QTOT'], color='green', label='Val')
@@ -30,22 +57,12 @@ def main():
 
 	scatter.show()
 
-	input_cols = ['CHK', 'PWH', 'PDC', 'TWH', 'FGAS', 'FOIL']
-	output_cols = ['QTOT']
+def print_evaluation(mse_value :float, mae_value :float, mape_value :float):
+	print(f'MSE: {mse_value}')
 
-	datahandler.set_input_cols(input_cols)
-	datahandler.set_output_cols(output_cols)
+	print(f'MAE: {mae_value}')
 
-	train_loader = datahandler.generate_dataloader(set='train')
-	val_loader = datahandler.generate_dataloader(set='val')
-
-	layers = [len(input_cols), 50, 50, len(output_cols)]
-	net = FCNN(layers)
-
-	n_epochs = 100
-	lr = 0.001
-	l2_reg = 0.001  # 10
-	net = train(net, train_loader, val_loader, n_epochs, lr, l2_reg)
+	print(f'MAPE: {mape_value} %')
 
 if __name__ == "__main__":
 	main()
